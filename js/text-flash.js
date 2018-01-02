80329977c17fd9a6c528fcb68896a9fbefb7b935
imgwho <imgwho@qq.com>
2018-01-02 20:09:32 +0800

    imgs

---
+++ js/text-flash.js
@@ -0,0 +1,45 @@
+function liquor(){
+var liquor=document.getElementById("text_liquor");
+
+liquor.style.display="none"; 
+
+}
+setTimeout("liquor()",1600);
+
+function tea(){
+	var tea=document.getElementById("text_tea");
+	tea.style.display="none";
+	
+}
+setTimeout("tea()",3000);
+
+function medicine(){
+	var medicine=document.getElementById("text_medicine");
+	medicine.style.display="none";
+}
+setTimeout("medicine()",4000);
+
+function fruit(){
+	var fruit=document.getElementById("text_fruit");
+	fruit.style.display="none";
+}
+setTimeout("fruit()",6000);
+
+
+function taste(){
+	var taste=document.getElementById("text_taste");
+	taste.style.display="none";
+}
+setTimeout("taste()",8000);
+
+function cuisine(){
+	var cuisine=document.getElementById("text_cuisine");
+	cuisine.style.display="none";
+}
+setTimeout("cuisine()",9900);
+function loading(){
+	var loading=document.getElementById("text-loading");
+	
+	loading.style.display="none";
+}
+setTimeout("loading()",12000);
